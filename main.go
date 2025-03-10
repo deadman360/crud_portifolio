a8c78773e9fd01847f608f22cb80a0959d38be25
deadman360 <vmartinho.kfg@gmail.com>
2025-03-10 20:03:54 +0000

    Logs

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/deadman360/crud_portifolio/src/configuration/logger"
 	"github.com/deadman360/crud_portifolio/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("Starting user aplication")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,19 +2,44 @@ package controller
 
 import (
 	"fmt"
+	"net/http"
 
-	"github.com/deadman360/crud_portifolio/src/configuration/rest_err"
+	"github.com/deadman360/crud_portifolio/src/configuration/logger"
+	"github.com/deadman360/crud_portifolio/src/configuration/validation"
 	"github.com/deadman360/crud_portifolio/src/controller/routes/model/request"
+	"github.com/deadman360/crud_portifolio/src/controller/routes/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap/zapcore"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zapcore.Field{
+			Key:    "journey",
+			String: "CreateUser",
+		})
 	var UserRequest request.UserRequest
 	if err := c.ShouldBindBodyWithJSON(&UserRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("Campos Incorretos", err.Error()))
+		logger.Error("Error trying to validate user info", err,
+			zapcore.Field{
+				Key:    "journey",
+				String: "CreateUser",
+			})
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 	fmt.Println(UserRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: UserRequest.Email,
+		Name:  UserRequest.Name,
+		Age:   UserRequest.Age,
+	}
+	logger.Info("User created sucessfully",
+		zapcore.Field{
+			Key:    "journey",
+			String: "CreateUser",
+		})
+	c.JSON(http.StatusOK, response)
 }
